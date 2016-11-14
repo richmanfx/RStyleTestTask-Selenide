b38f7d3c5337f36823ed4bb1aed404815fa08fd4
richmanfx <richmanfx@yandex.ru>
2016-11-14 07:52:14 +0300

    Раскрытие меню по HOVER загнал в цикл с проверкой.

--- src/main/java/ServiceOrder.java
+++ src/main/java/ServiceOrder.java
@@ -4,6 +4,7 @@
  */
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementNotVisibleException;
 
@@ -32,8 +33,8 @@ public class ServiceOrder {
                                 "resources" + File.separator + "documents";
         File uploadedFile = new File(filePath + File.separator + fileName);
 
-//        Configuration.browserSize = "1880x800";
-//        Configuration.timeout = 8000;
+        Configuration.browserSize = "1880x800";
+        Configuration.timeout = 2000;
 
         // Использовать Chrome.
         final String PATH_TO_CHROMEDRIVER_EXE = "src" + File.separator +
@@ -44,29 +45,26 @@ public class ServiceOrder {
         System.setProperty("webdriver.chrome.driver", PATH_TO_CHROMEDRIVER_EXE);
         Configuration.browser = "chrome";
 
-
+        // Временно без Гугля, для скорости
+        open(searchSite);
 //        open("http://" + searchServer);                             // Открываем google.com
 //        $(By.name("q")).setValue(searchPhrase1).pressEnter();       // Находим "performance lab"
 //        $(By.xpath("//a[@href = '" + searchSite + "']")).click();   // Переходим на www.performance-lab.ru
 
-        // Временно без гугля
-        open(searchSite);
+        // Часто не разворачивает и сразу сворачивает меню
+        SelenideElement element;
+         do {
+            element = $(By.xpath("//li[@id='menu-item-317']/a[@href='#' and contains(text(),'" + searchPhrase2 + "')]")).hover();
 
-        // Часто не разворачивает меню
-        $(By.xpath("//li[@id='menu-item-317']/a[@href='#' and contains(text(),'" + searchPhrase2 + "')]")).hover();
-
-//        if(!$(By.xpath("//li[@id='menu-item-957']//a[contains(text(),'Автоматизация тестирования')]")).isDisplayed()) {
-//            System.out.println("Второй заход с 'hover'.");
-//            $(By.xpath("//li[@id='menu-item-317']/a[@href='#' and contains(text(),'" + searchPhrase2 + "')]")).hover();
-//        }
-
-        // Ссылка видна, она одна, но при клике становится невидна - печаль, не победил пока
+        // Ссылка видна, она единственная, но при клике становится невидна - печаль, не победил пока, сделал обход
 //        $(By.xpath("//li[@id='menu-item-957']//a[contains(text(),'Автоматизация тестирования')]")).isDisplayed();
 //        $(By.xpath("//li[contains(@id, 'menu-item-957')]//a[contains(text(),'Автоматизация тестирования')]")).click();
 //        $$(By.xpath("//li[@id='menu-item-957']//a[contains(text(),'Автоматизация тестирования')]")).shouldHave(size(1));
 
-        // Обход
-        $(By.xpath("//a[@href='/software-testing/']")).click();
+
+         } while (!$(By.xpath(".//p/a[contains(text(), 'Посмотреть все услуги')]")).isDisplayed()); // В обход
+
+        $(By.xpath(".//p/a[contains(text(), 'Посмотреть все услуги')]")).click();
         $(By.xpath("//article[@id='post-929']//a[text()='" + searchPhrase3 + "']")).click();
 
         // Кнопка "Заказать услугу"
@@ -79,11 +77,14 @@ public class ServiceOrder {
         $(By.name("your-phone")).setValue(clientPhone);
         $(By.name("your-company")).setValue(clientCompany);
 
-//        $(By.xpath("//li[@id='menu-item-141']/input[text()='" + searchPhrase3 + "']")).click();
+        // Чекбокс выбора услуги
         $(By.xpath(".//input[@type='checkbox' and @value='" + searchPhrase3 + "']")).click();
+
+        // Загрузка файла
         $(By.name("technical-documentation")).uploadFile(uploadedFile);
 
-        $(By.xpath(".//input[@type='submit' and @value='Отправить заявку']")).click();
+        // Кнопка отправки заявки
+        $(By.xpath(".//input[@type='submit']")).click();
 
 
 
